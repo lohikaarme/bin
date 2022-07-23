a5c3511aad9fd161e3d5ce8cc5d7454e6a9967e2
lohikaarme <stewart.watt@gmail.com>
2022-07-23 11:34:28 -0700

    Changed rbb_method to load page once per tag scan, added timer

--- rbb_methods.rb
+++ rbb_methods.rb
@@ -11,12 +11,17 @@ end
 
 sites = [ "http://www.wsj.com", "http://www.nytimes.com", "http://www.ft.com" ]
 tags = ["div","h1","h2","h3","img","p"]   
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
 sites.each do |url|
     puts "#{url}"
+    page = fetch_page(url)
     tags.each do |tag|
-        page = fetch_page(url)
         tag_count = count_image_tags(page, tag)
         puts "\t - #{tag_count} <#{tag}> tags"
     end
 end
+
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  elapsed = ending - starting
+  puts "elapsed time = #{elapsed}"
